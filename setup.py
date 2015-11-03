6ebee98821b3d2eb076d9a864194f8f4112851c4
UltrasoundSam <samuel.hill@warwick.ac.uk>
2015-11-03 23:52:11 +0000

    Modifies and updates setup file

--- setup.py
+++ setup.py
@@ -3,18 +3,10 @@
 from distutils.core import setup
 
 setup(name='metaArray',
-      version='0.9.2',
+      version='0.9.3',
       description='meta-info container for numpy ndarray, with associate libraries.',
       author='Charley (Yichao) Fan',
       author_email='y.fan@warwick.ac.uk',
       url='http://warwickultrasound.co.uk/',
       packages=['metaArray', 'distutils.command'],
      )
-     
-     
-from distutils.core import setup
-setup(name='foo',
-      version='1.0',
-      py_modules=['foo'],
-      )
-
