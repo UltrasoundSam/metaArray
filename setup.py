6551766d3eda39f92967ebef9d0b78c03f5134cc
UltrasoundSam <samuel.hill@warwick.ac.uk>
2015-11-05 12:30:02 +0000

    Adds MANIFEST.in file so that setup.py can build distributions

--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ def read(fname):
 
 
 setup(name='metaArray',
-      version='0.9.9',
-      description='meta-info container for numpy ndarray, with associate libraries.',
-      author='Charley (Yichao) Fan',
-      author_email='y.fan@warwick.ac.uk',
-      url='http://warwickultrasound.co.uk/',
-      packages=['metaArray'],
-      long_description=read('README.md'),
+	version='0.9.9',
+	description='meta-info container for numpy ndarray, with associate libraries.',
+	author='Charley (Yichao) Fan',
+	author_email='y.fan@warwick.ac.uk',
+	url='http://warwickultrasound.co.uk/',
+	packages=['metaArray'],
+	long_description=read('README'),
      )
