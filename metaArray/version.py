5fecc9434c662ad55560e372d790ca0bfeb6bcbc
UltrasoundSam <samuel.hill@warwick.ac.uk>
2016-09-23 16:35:57 +0100

    Updates version number

--- metaArray/version.py
+++ metaArray/version.py
@@ -2,8 +2,8 @@
 metaArray version info
 '''
 short_version = '1.1'
-version = '1.1.0'
-full_version = '1.1.0 2016-03-18'
+version = '1.1.1'
+full_version = '1.1.1 2016-09-23'
 release = True
 
 if not release:
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 
 setup(name='metaArray',
-	version='1.1.0',
+	version='1.1.1',
 	description='meta-info container for numpy ndarray, with associate libraries.',
 	author='Charley (Yichao) Fan',
 	author_email='y.fan@warwick.ac.uk',
