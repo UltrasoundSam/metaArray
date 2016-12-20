27692dfa88eb1575265ad6eacc413ef881b805e4
UltrasoundSam <samuel.hill@warwick.ac.uk>
2016-12-20 10:21:30 +0000

    Update version number

--- metaArray/version.py
+++ metaArray/version.py
@@ -2,8 +2,8 @@
 metaArray version info
 '''
 short_version = '1.1'
-version = '1.1.4'
-full_version = '1.1.4 2016-11-14'
+version = '1.1.5'
+full_version = '1.1.5 2016-12-20'
 release = True
 
 if not release:
