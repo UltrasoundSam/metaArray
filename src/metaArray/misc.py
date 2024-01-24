f6363fbea6c3f0d298e5fcebba5dc66accbf8a9a
Sam Hill <sam.j.hill@protonmail.com>
2024-01-24 20:40:19 +0000

    Tidy up examples.py

--- src/metaArray/misc.py
+++ src/metaArray/misc.py
@@ -26,6 +26,7 @@ Package dependency:
 '''
 import numpy as np
 import typing
+import os
 
 
 def linearFunc(x0: float, y0: float, x1: float,
@@ -73,3 +74,51 @@ def expFunc(x0: float, y0: float, x1: float,
     k = (y1 - y0) / (x1 - x0)
     b = ((y0 - k*x0) + (y1 - k*x1)) / 2
     return lambda x: base ** (k * x + b)
+
+
+class filePath:
+    """
+    FilePath object, provide various information about the file path
+
+    currently provide the following:
+
+    self.full       The full path, i.e. the input string
+    self.baseDir    The dir path to the file
+    self.name       The file name
+    self.ext        The file extension
+    self.read
+    self.write
+    self.exist      Whether it already exists
+    """
+
+    def __init__(self, file_path: str) -> None:
+
+        self.full = path = os.path.abspath(file_path.strip())
+        self.read = os.access(path, os.R_OK)
+        self.write = os.access(path, os.W_OK)
+        self.execute = os.access(path, os.X_OK)
+        self.exist = os.access(path, os.F_OK)
+        self.baseDir, fname = os.path.split(path)
+
+        # Modify the behaviour if the file does not exit, check if the directory
+        # is writable instead
+        if not self.exist:
+            self.write = os.access(self.baseDir, os.W_OK)
+
+        self.name, self.ext = os.path.splitext(fname)
+
+    def __repr__(self) -> str:
+        """
+        Text representation of the object
+        """
+        desc = ''
+        desc += 'Full: ' + self.full + os.linesep
+        desc += 'Name: ' + self.name + os.linesep
+        desc += 'Read: ' + str(self.read) + os.linesep
+        desc += 'Write: ' + str(self.write) + os.linesep
+        desc += 'Execute: ' + str(self.execute) + os.linesep
+        desc += 'Exist: ' + str(self.exist) + os.linesep
+        desc += 'baseDir: ' + self.baseDir + os.linesep
+        desc += 'extension: ' + self.ext + os.linesep
+
+        return desc
