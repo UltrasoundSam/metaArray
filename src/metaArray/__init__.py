6bef69889ccd966eeaecaf103eb8c270fefe5c58
Sam Hill <sam.j.hill@protonmail.com>
2024-01-29 14:25:57 +0000

    Moves example datafiles to new position

--- src/metaArray/__init__.py
+++ src/metaArray/__init__.py
@@ -1,7 +1,6 @@
 # Setting out version number
 __version__ = '2.0.0'
 
-
 # Import required functionality
 from .core import metaArray
 
@@ -16,16 +15,17 @@ from .misc import cplx_trig_func, MotherMorlet, spline_resize
 # metaArray aware Plotting lib (based on matplotlib)
 from .drv_pylab import plot1d, plot2d, plotcomplex, plotcomplexpolar
 
-# File I/O
-from .drv_h5py import to_h5, from_h5
-from .drv_Tek import isf, DPO2000_csv, TDS2000_csv, DPO2000_isf
-
-# External devices
-from .drv_ritec import Ritec4000
-from .drv_hp4294 import HP4294A
-
 # metaArray aware numerical transforms
 from .metaTrans import rfft, stfft, cwt
 
 # metaArray aware functions
 from .metaFunc import meta_resample, padding_calc, meta_lowpass
+
+# File I/O
+from .drv_h5py import to_h5, from_h5
+from .drv_Tek import isf, DPO2000_csv, TDS2000_csv, DPO2000_isf
+from .drv_flex import pout_hist, data_out1
+
+# External devices
+from .drv_ritec import Ritec4000
+from .drv_hp4294 import HP4294A
